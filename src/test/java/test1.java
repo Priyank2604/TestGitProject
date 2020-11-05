4f1a51bb3d4b54f079bd6b901f7fc2d24ed4316a
Priyank <priyank.tailor2604@gmail.com>
2020-11-05 14:35:46 +0530

    test second commit by development branch

--- src/test/java/test1.java
+++ src/test/java/test1.java
@@ -2,6 +2,5 @@ public class test1 {
     public static void main(String[] args) {
         System.out.println("Test1");
         System.out.println("Test2");
-        System.out.println("Test3");
     }
 }
